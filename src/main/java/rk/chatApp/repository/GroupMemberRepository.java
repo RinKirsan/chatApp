ed864bd1ae05dc824801103d9d19f90e6533db67
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2025-02-11 21:47:09 +0400

    1

--- src/main/java/rk/chatApp/repository/GroupMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package rk.chatApp.repository;
-
-import rk.chatApp.model.GroupMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
-    boolean existsByGroupIdAndUserId(Long groupId, Long userId);
-}
