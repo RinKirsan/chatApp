76dec095e75dffa36750f41ed7efc97aedecb512
RinKirsan <116436137+RinKirsan@users.noreply.github.com>
2025-02-14 22:59:14 +0400

    create errorPage

---
+++ src/main/java/rk/chatApp/service/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package rk.chatApp.service;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleRuntimeException(RuntimeException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "errorPage";
+    }
+
+}
+
